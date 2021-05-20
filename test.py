44d65d262a6effe06d5c50b34aecd32f68c0b9f4
tachangshuang <tachangshuang@rtmap.com>
2021-05-20 19:12:16 +0800

    Q20

--- test.py
+++ test.py
@@ -1,1 +1,1 @@
-def fibonacci(n):    if n == 0 or n == 1 or n == 2:        return n    f1, f2, f3 = 1, 2, 3    i = 3    while i <= n:        f3 = f1 + f2        f1 = f2        f2 = f3        i += 1    return f3print(fibonacci(4))print("@@@@@@@@@@@@@@@@@@@@")
+def fibonacci(n):    if n == 0 or n == 1 or n == 2:        return n    f1, f2, f3 = 1, 2, 3    i = 3    while i <= n:        f3 = f1 + f2        f1 = f2        f2 = f3        i += 1    return f3print(fibonacci(4))print("@@@@@@@@@@@@@@@@@@@@@")
